640ecfbc5351bb05a5bc916be712b740bd9ee778
Huang Kai <huangkai23@mails.jlu.edu.cn>
2025-04-04 16:29:04 +0800

    Parareal

--- Parareal.cpp
+++ Parareal.cpp
@@ -1,4 +1,37 @@
-
+/*
+* ----------------------------------------------------------------------------
+ * Project Name : Parareal
+ * File         : Parareal.cpp
+ * Author       : Nero <Huangkai23@mails.jlu.edu.cn>
+ * Created      : 2025-04-04
+ * Description  :   PARAREAL implementation of the parareal algorithm
+ * U=Parareal(F,G,T,u0,N,K); applies the parareal algorithm with fine
+ * solver F(t0,t1,ut0) and coarse solver G(t0,t1,ut0) on [0,T] with
+ * initial condition u0 at t=0 using N equidistant coarse time points
+ * doing K iterations. The output U{k} contains the parareal
+ * approximations at the coarse time points for each iteration k.
+ *
+ * Copyright (c) 2025 Nero
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * ----------------------------------------------------------------------------
+ */
 #include <iostream>
 #include <mkl.h>
 #include <mkl_service.h>
